cdd486f0390eb5c9413fe4ce46dbe11b479b5646
karl <kimwz.kr@gmail.com>
2017-06-05 18:57:35 +0900

    update config

--- example/config.py
+++ example/config.py
@@ -1,5 +1,5 @@
 CONFIG = {
-    'FACEBOOK_TOKEN': 'EAANgzn2wUtoBADwVamrsDaGZBRrFiEKz7UXtpALkmiH1WNzBPZCtbvbSsDGGZAbmBbdJZChg7ivMMdURQAwiRCsoiB41LDC8haMUzSutGti88ebZBx5smzLpR0xMzjlsfetrrCdWdcjTq8Onhr0bbZB11l67YKuV0OpQpalmFgLQZDZD',
-    'VERIFY_TOKEN': 'verify_simsimbot',
-    'SERVER_URL': 'https://karl.ohrocks.com'
+    'FACEBOOK_TOKEN': '###',
+    'VERIFY_TOKEN': 'verify_token',
+    'SERVER_URL': 'https://yourdomain'
 }
